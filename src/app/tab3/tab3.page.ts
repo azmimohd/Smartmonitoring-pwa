c6aa9ce426be2bf87fb8d1ce026db64d4c98c96e
mazmimohd <49468781+azmimohd@users.noreply.github.com>
2022-01-19 21:23:24 +0800

    Update tab3.page.ts
    selection day feature

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
 import { ToastController, NavController, AlertController } from '@ionic/angular';
-import { Chart } from 'chart.js';
+
 import { AngularFireObject } from '@angular/fire/compat/database';
 import { Observable } from 'rxjs';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
@@ -9,8 +9,9 @@ import * as moment from 'moment';
 import { Subscription } from 'rxjs';
 import { timer } from 'rxjs';
 import { map } from 'rxjs/operators';
-import {LocalNotifications} from "@capacitor/local-notifications"
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as ChartAnnotation from 'chartjs-plugin-annotation';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -22,6 +23,9 @@ import {LocalNotifications} from "@capacitor/local-notifications"
 
 export class Tab3Page implements OnInit {
   public dayToday : string
+  public counter : number
+  public resetSelect
+  public sensor
   timerSubscription: Subscription;
   itemRef: AngularFireObject<any>;
   airsensor: Observable<any>;
@@ -41,267 +45,284 @@ export class Tab3Page implements OnInit {
   bars: any;
   colorArray: any;
   
+  
     
   constructor(
     private toastController: ToastController,
     private navCtrl: NavController,
-    public db: AngularFireDatabase
+    public db: AngularFireDatabase,
+    public toastCtrl:ToastController,
+    public auth: AngularFireAuth
     
     ) {
-      this.airsensor = db.object('airsensor').valueChanges();
+
+      
       
   }
 
-  ngOnInit() {
-
+  getWatt(){
     
-    this.timerSubscription = timer(0, 500000).pipe(
-      map(() => {
-    const today = moment()
-    const dayFormat = today.format("dddd");
-    this.dayToday = dayFormat
-    
-    console.log(dayFormat);
-    const Wattval = '/chart/Watt/'+this.dayToday
-    const Tempval = '/chart/Temp/'+this.dayToday
-    const Humival = '/chart/Humi/'+this.dayToday
-    const Airval = '/chart/Air/'+this.dayToday
-    const timeNow = '/chart/Time/'+this.dayToday
-    console.log(Wattval)
-     console.log(Wattval)
-    this.itemRef = this.db.object(timeNow);
-    this.itemRef.snapshotChanges().subscribe(Rtime => {
-    
-    this.itemRef = this.db.object(Wattval);
-    this.itemRef.snapshotChanges().subscribe(Rwatt => {
-      const watt = Rwatt.payload.val()
-       console.log(watt)
-    this.itemRef = this.db.object(Tempval);
-    this.itemRef.snapshotChanges().subscribe(Rtemp => {
+    this.auth.onAuthStateChanged(user => {
+      if (user) {
+        //user.uid
+       // console.log(user.uid)
+      // var userid = user.uid
 
-    this.itemRef = this.db.object(Humival);
-    this.itemRef.snapshotChanges().subscribe(Rhumi => {
-
-    this.itemRef = this.db.object(Airval);
-    this.itemRef.snapshotChanges().subscribe(Rair => {
-
+       this.timerSubscription = timer(0, 500000).pipe(
+        map(() => {
+      const today = moment()
+      const dayFormat = today.format("dddd");
+      //this.dayToday = dayFormat
+      
+      console.log(dayFormat);
+      const Wattval = user.uid+'/chart/Watt/'+this.dayToday
+      const Tempval = user.uid+'/chart/Temp/'+this.dayToday
+      const Humival = user.uid+'/chart/Humi/'+this.dayToday
+      const Airval = user.uid+'/chart/Air/'+this.dayToday
+      const timeNow = user.uid+'/chart/Time/'+this.dayToday
+      console.log(Wattval)
+       console.log(Wattval)
+      this.itemRef = this.db.object(timeNow);
+      this.itemRef.snapshotChanges().subscribe(Rtime => {
+      
+      this.itemRef = this.db.object(Wattval);
+      this.itemRef.snapshotChanges().subscribe(Rwatt => {
+        const watt = Rwatt.payload.val()
+         console.log(watt)
+      this.itemRef = this.db.object(Tempval);
+      this.itemRef.snapshotChanges().subscribe(Rtemp => {
   
-      this.itemRef = this.db.object('/chart/trig/day');
-      this.itemRef.snapshotChanges().subscribe(trig => {
-
-        this.itemRef = this.db.object('/chart/trig/currentDay');
-        this.itemRef.snapshotChanges().subscribe(curentDay => {
-     console.log(Rtemp.payload.val())
-     console.log(Rtime.payload.val())
-      this.bars = new Chart(this.linesChart2.nativeElement, {
-        type: 'line',
-        backgroundColor: "rgba(255, 0, 255, 0)",
-        data: {
-          labels: ['12Am', '1Am', '2Am', '3Am', '4Am', '5Am', '6Am','7Am', '8Am', '9Am', '10Am', '11Am', '12Pm', '1Pm','2Pm', '3Pm', '4Pm', '5Pm', '6Pm', '7Pm', '8Pm','9Pm', '10Pm', '11Pm',],
-          datasets: [{
-            backgroundColor: "#40BFCF",
-            pointBackgroundColor: "#40BFCF",
-            pointBorderColor: "#40BFCF",
-            borderColor: "#40BFCF",
-            data: Rwatt.payload.val(),
-            fill: true,
-          }]
-        },
-        options: {
-          responsive: true,
-          legend: {
-            display: false
-          },
-          tooltips: {
-            enabled: true
-          },
-          scales: {
-            xAxes: [{
-              gridLines: {
-                display: false
-              }
-            }],
-            yAxes: [{
-              gridLines: {
-                display: false
-              },
-              ticks: {
-                display: true
-              }
-            }]
-          }
-        }
-      });
-
-      this.bars = new Chart(this.linesChart3.nativeElement, {
-        type: 'line',
-        backgroundColor: "rgba(255, 0, 255, 0)",
-        data: {
-          labels: ['12Am', '1Am', '2Am', '3Am', '4Am', '5Am', '6Am','7Am', '8Am', '9Am', '10Am', '11Am', '12Pm', '1Pm','2Pm', '3Pm', '4Pm', '5Pm', '6Pm', '7Pm', '8Pm','9Pm', '10Pm', '11Pm',],
-          datasets: [{
-            backgroundColor: "#40BFCF",
-            pointBackgroundColor: "#40BFCF",
-            pointBorderColor: "#40BFCF",
-            borderColor: "#40BFCF",
-            data: Rtemp.payload.val(),
-            fill: true,
-          }]
-        },
-        options: {
-          responsive: true,
-          legend: {
-            display: false
-          },
-          tooltips: {
-            enabled: false
-          },
-          scales: {
-            xAxes: [{
-              gridLines: {
-                display: false
-              }
-            }],
-            yAxes: [{
-              gridLines: {
-                display: false
-              },
-              ticks: {
-                display: true
-              }
-            }]
-          }
-        }
-      });
-
-
-      this.bars = new Chart(this.linesChart4.nativeElement, {
-        type: 'line',
-        backgroundColor: "rgba(255, 0, 255, 0)",
-        data: {
-          labels: ['12Am', '1Am', '2Am', '3Am', '4Am', '5Am', '6Am','7Am', '8Am', '9Am', '10Am', '11Am', '12Pm', '1Pm','2Pm', '3Pm', '4Pm', '5Pm', '6Pm', '7Pm', '8Pm','9Pm', '10Pm', '11Pm',],
-          datasets: [{
-            backgroundColor: "#40BFCF",
-            pointBackgroundColor: "#40BFCF",
-            pointBorderColor: "#40BFCF",
-            borderColor: "#40BFCF",
-            data: Rhumi.payload.val(),
-            fill: true,
-          }]
-        },
-        options: {
-          responsive: true,
-          legend: {
-            display: false
-          },
-          tooltips: {
-            enabled: false
-          },
-          scales: {
-            xAxes: [{
-              gridLines: {
-                display: false
-              }
-            }],
-            yAxes: [{
-              gridLines: {
-                display: false
-              },
-              ticks: {
-                display: true
-              }
-            }]
-          }
-        }
-      });
-
-
-      this.bars = new Chart(this.linesChart5.nativeElement, {
-        type: 'line',
-        backgroundColor: "rgba(255, 0, 255, 0)",
-        data: {
-          labels: ['12Am', '1Am', '2Am', '3Am', '4Am', '5Am', '6Am','7Am', '8Am', '9Am', '10Am', '11Am', '12Pm', '1Pm','2Pm', '3Pm', '4Pm', '5Pm', '6Pm', '7Pm', '8Pm','9Pm', '10Pm', '11Pm',],
-          datasets: [{
-            backgroundColor: "#40BFCF",
-            pointBackgroundColor: "#40BFCF",
-            pointBorderColor: "#40BFCF",
-            borderColor: "#40BFCF",
-            data: Rair.payload.val(),
-            fill: true,
-          }]
-        },
-        options: {
-          responsive: true,
-          legend: {
-            display: false
-          },
-          tooltips: {
-            enabled: false
-          },
-          scales: {
-            xAxes: [{
-              gridLines: {
-                display: false
-              }
-            }],
-            yAxes: [{
-              gridLines: {
-                display: false
-              },
-              ticks: {
-                display: true
-              }
-            }]
-          }
-        }
-      });
-      
-const WattvalDay = '/wattUsage/'+dayFormat     
-const todayUsage = watt.reduce((a, b) => a + b, 0)
-var usage = parseInt(todayUsage)
-console.log(usage)
-const itemRef = this.db.object(WattvalDay);
-itemRef.update({ day : usage}); 
+      this.itemRef = this.db.object(Humival);
+      this.itemRef.snapshotChanges().subscribe(Rhumi => {
+  
+      this.itemRef = this.db.object(Airval);
+      this.itemRef.snapshotChanges().subscribe(Rair => {
+  
     
-
-      //usage in 24 hour
-     // console.log(watt.reduce((a, b) => a + b, 0))
-
-
-
-    /*  LocalNotifications.requestPermissions().then((Permissions)=>{
-        if(Permissions.display == 'granted'){
-          LocalNotifications.schedule({
-            notifications: [
-              {
-                title: "Hi Here Your Today Update",
-                body: "your usage "+usage ,
-                id: 1,
-                smallIcon: 'house',
-                actionTypeId: 'OPEN_PRODUCT',
+        this.itemRef = this.db.object(user.uid+'/chart/trig/day');
+        this.itemRef.snapshotChanges().subscribe(trig => {
+  
+          this.itemRef = this.db.object(user.uid+'/chart/trig/currentDay');
+          this.itemRef.snapshotChanges().subscribe(curentDay => {
+       console.log(Rtemp.payload.val())
+       console.log(Rtime.payload.val())
+        this.bars = new Chart(this.linesChart2.nativeElement, {
+          type: 'line',
+          backgroundColor: "rgba(255, 0, 255, 0)",
+          data: {
+            labels: ['12Am', '1Am', '2Am', '3Am', '4Am', '5Am', '6Am','7Am', '8Am', '9Am', '10Am', '11Am', 
+            '12Pm', '1Pm','2Pm', '3Pm', '4Pm', '5Pm', '6Pm', '7Pm', '8Pm','9Pm', '10Pm', '11Pm',],
+            datasets: [{
+              backgroundColor: "#40BFCF",
+              pointBackgroundColor: "#40BFCF",
+              pointBorderColor: "#40BFCF",
+              borderColor: "#40BFCF",
+              data: Rwatt.payload.val(),
+              fill: false,
+            }]
+          },
+          options: {
+            responsive: true,
+            legend: {
+              display: false
+            },
+            tooltips: {
+              enabled: true
+            },
+            scales: {
+              xAxes: [{
+                gridLines: {
+                  display: true
+                }
                 
-                schedule: {
-                  every: "minute"
+              }],
+              yAxes: [{
+                gridLines: {
+                  display: true
                 },
-      
-                extra: null
+                ticks: {
+                  display: true
+                },
+              }]
+            }
+          },annotation: {
+            annotations: [
+              {
+                drawTime: 'afterDatasetsDraw',
+                id: 'hline',
+                type: 'line',
+                mode: 'horizontal',
+                scaleID: 'y-axis-0',
+                value: 300, // My value
+                borderColor: 'red',
+                borderWidth: 3,
+                label: {
+                   backgroundColor: 'red',
+                   enabled: true
+                }
               }
             ]
-          });
-        }
-      })*/
+          }
 
+
+    
+        });
+  
+        this.bars = new Chart(this.linesChart3.nativeElement, {
+          type: 'line',
+          backgroundColor: "rgba(255, 0, 255, 0)",
+          data: {
+            labels: ['12Am', '1Am', '2Am', '3Am', '4Am', '5Am', '6Am','7Am', '8Am', '9Am', '10Am', '11Am', 
+            '12Pm', '1Pm','2Pm', '3Pm', '4Pm', '5Pm', '6Pm', '7Pm', '8Pm','9Pm', '10Pm', '11Pm',],
+            datasets: [{
+              backgroundColor: "#40BFCF",
+              pointBackgroundColor: "#40BFCF",
+              pointBorderColor: "#40BFCF",
+              borderColor: "#40BFCF",
+              data: Rtemp.payload.val(),
+              fill: false,
+            }]
+          },
+          options: {
+            responsive: true,
+            legend: {
+              display: false
+            },
+            tooltips: {
+              enabled: true
+            },
+            scales: {
+              xAxes: [{
+                gridLines: {
+                  display: true
+                }
+              }],
+              yAxes: [{
+                gridLines: {
+                  display: true
+                },
+                ticks: {
+                  display: true
+                }
+              }]
+            }
+          }
+        });
+  
+  
+        this.bars = new Chart(this.linesChart4.nativeElement, {
+          type: 'line',
+          backgroundColor: "rgba(255, 0, 255, 0)",
+          data: {
+            labels: ['12Am', '1Am', '2Am', '3Am', '4Am', '5Am', '6Am','7Am', '8Am', '9Am', '10Am', '11Am', '12Pm', '1Pm','2Pm', '3Pm', '4Pm', '5Pm', '6Pm', '7Pm', '8Pm','9Pm', '10Pm', '11Pm',],
+            datasets: [{
+              backgroundColor: "#40BFCF",
+              pointBackgroundColor: "#40BFCF",
+              pointBorderColor: "#40BFCF",
+              borderColor: "#40BFCF",
+              data: Rhumi.payload.val(),
+              fill: false,
+            }]
+          },
+          options: {
+            responsive: true,
+            legend: {
+              display: false
+            },
+            tooltips: {
+              enabled: true
+            },
+            scales: {
+              xAxes: [{
+                gridLines: {
+                  display: true
+                }
+              }],
+              yAxes: [{
+                gridLines: {
+                  display: true
+                },
+                ticks: {
+                  display: true
+                }
+              }]
+            }
+          }
+        });
+  
+  
+        this.bars = new Chart(this.linesChart5.nativeElement, {
+          type: 'line',
+          backgroundColor: "rgba(255, 0, 255, 0)",
+          data: {
+            labels: ['12Am', '1Am', '2Am', '3Am', '4Am', '5Am', '6Am','7Am', '8Am', '9Am', '10Am', '11Am', '12Pm', '1Pm','2Pm', '3Pm', '4Pm', '5Pm', '6Pm', '7Pm', '8Pm','9Pm', '10Pm', '11Pm',],
+            datasets: [{
+              backgroundColor: "#40BFCF",
+              pointBackgroundColor: "#40BFCF",
+              pointBorderColor: "#40BFCF",
+              borderColor: "#40BFCF",
+              data: Rair.payload.val(),
+              fill: false,
+            }]
+          },
+          options: {
+            responsive: true,
+            legend: {
+              display: false
+            },
+            tooltips: {
+              enabled: true
+            },
+            scales: {
+              xAxes: [{
+                gridLines: {
+                  display: true
+                }
+              }],
+              yAxes: [{
+                gridLines: {
+                  display: true
+                },
+                ticks: {
+                  display: true
+                }
+              }]
+            }
+          }
+        });
+        
+  const WattvalDay = user.uid+'/wattUsage/'+dayFormat     
+  const todayUsage = watt.reduce((a, b) => a + b, 0)
+  var usage = parseInt(todayUsage)
+  console.log(usage)
+  const itemRef = this.db.object(WattvalDay);
+  itemRef.update({ day : usage}); 
+      
+  
+      });
     });
   });
-});
-});
-});
-});
-
-
-
   });
+  });
+  });
+  
+  
+  
+    });
+  })
+  ).subscribe(); 
+      }
+      else {
+        this.showToast("Please Login First")
+
+      }
 })
-).subscribe();   
+
+
+  }
+
+  ngOnInit(): void {
+    Chart.pluginService.register(ChartAnnotation);
   }
 
 
@@ -309,8 +330,47 @@ itemRef.update({ day : usage});
 
 
 
+graphReset(){
+
+  this.auth.onAuthStateChanged(user => {
+    if (user) {
+     // user.uid
+     // console.log(user.uid)
+     //var userid = user.uid
+
+     this.showToast("Data Collection for "+this.sensor+"/"+this.resetSelect+" has been deleted!!")
+     for(this.counter=0;this.counter<=23;this.counter++){
+       const select = user.uid+'/chart/'+this.sensor+'/'+this.resetSelect+'/'+this.counter
+       const itemRef = this.db.object(select);
+       itemRef.set(0);
+       
+     }
+    }
+    else {
+      this.showToast("Please Login First")
+
+    }
+})
 
 }
+async showToast(msg) {
+  const toast = await this.toastCtrl.create({
+    header: msg,
+    position: 'top',
+    duration: 4000
+  });
+  toast.present();
+}
+}
+
+
+
+
+function rgba(arg0: number, arg1: number, arg2: number, arg3: number) {
+  throw new Error('Function not implemented.');
+}
+
+
 
 
 
